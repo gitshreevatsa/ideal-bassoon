05b43474493a3d1c7093b4c3103f8c23cb07b121
Shree Vatsa <vastas2002@gmail.com>
2024-03-24 09:34:33 +0530

    contracts added

--- contractIntegration.js
+++ contractIntegration.js
@@ -1,9 +1,9 @@
 const {ethers} = require('ethers');
 const abi = require('./abi.json');
 
-const provider = new ethers.JsonRpcProvider('https://rpc2.sepolia.org');
+const provider = new ethers.JsonRpcProvider('https://rpc.ankr.com/scroll_sepolia_testnet');
 
-const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const contractAddress = '0x1800c4735B38e61B3C8Ad10C304016873d79c8BC';
 
 const contract = new ethers.Contract(contractAddress, abi, provider);
 
